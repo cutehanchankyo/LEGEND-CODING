e6fec2fd840b535fcb628844603f01c2c693063d
cutehanchankyo <jeongchangyo71@gmail.com>
2023-02-01 22:50:49 +0900

    add Controller GetMapping

--- src/main/java/com/example/demo/domain/presentation/MemberController.java
+++ src/main/java/com/example/demo/domain/presentation/MemberController.java
@@ -3,16 +3,29 @@ package com.example.demo.domain.presentation;
 import com.example.demo.domain.presentation.dto.request.MemberRequest;
 import com.example.demo.domain.service.MemberService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/board")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping
+    public ResponseEntity view(){
+        memberService.viewALl();
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping("/{seq}")
+    public ResponseEntity search(Long seq){
+        memberService.viewOne(seq);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 
     @PostMapping("/add")
     public ResponseEntity<Void> add(MemberRequest request){
