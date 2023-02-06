78d899364a8dca7eb49c59492f73ef91ee198a27
cutehanchankyo <jeongchangyo71@gmail.com>
2023-02-06 16:32:19 +0900

    add Service

--- src/main/java/com/example/demo/domain/security/service/UserService.java
+++ src/main/java/com/example/demo/domain/security/service/UserService.java
@@ -1,4 +1,8 @@
 package com.example.demo.domain.security.service;
 
+
+import com.example.demo.domain.security.presentation.dto.request.UserRequest;
+
 public interface UserService {
+    public boolean save(UserRequest userRequest);
 }
